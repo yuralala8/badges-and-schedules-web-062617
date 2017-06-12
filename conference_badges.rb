50baee2f4d65c6cde12b949360e6d7067e0fe601
yuralala8 <yc1435@nyu.edu>
2017-06-12 19:19:20 -0400

    badges and schedules commit

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+    batch_badge = []
+    
+    name_array.each do |name|
+        batch_badge << badge_maker(name)
+    end
+    batch_badge
+end
+
+def assign_rooms(speakers)
+    rooms = []
+    
+    speakers.each_with_index do |speakers, index|
+        rooms << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
+        end
+    rooms
+end
+
+def printer(speakers)
+    batch_badge = batch_badge_creator(speakers)
+        batch_badge.each { |x| puts x }
+    rooms = assign_rooms(speakers)
+    rooms.each { |y| puts y}
+end
